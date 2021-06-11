4241fb54e554bdb707e999e7ea0b910ec2f1c589
OOF <oof@motualis.com>
2021-06-11 13:10:06 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,42 @@
+var moment = require('moment-timezone');
+
+module.exports = function(eleventyConfig) {
+	
+	/* Folders allowed through to SITE on build */
+	eleventyConfig.addPassthroughCopy("admin");
+	eleventyConfig.addPassthroughCopy("posts");
+	
+	eleventyConfig.addPassthroughCopy("assets");
+	eleventyConfig.addPassthroughCopy("styles");
+	eleventyConfig.addPassthroughCopy("scripts");
+	
+	
+	// Format the Date
+	eleventyConfig.addFilter("dateformat", function(dateIn) {
+		return moment(dateIn).tz('EST').format('MM.DD.YYYY');
+    });
+    
+    // Get the first `n` elements of a collection. 
+	eleventyConfig.addFilter("head", (array, n) => {
+		if( n < 0 ) {
+			return array.slice(n);
+		}
+	
+		return array.slice(0, n);
+	});
+	
+	// Use only a portion of an array. 
+	eleventyConfig.addFilter("portion", (array, f, l) => {
+		return array.slice(f, l);
+	});
+	
+	return {
+		
+		dir: {
+			input: ".",
+			output: "_site",
+			includes: "_includes",
+			data: "_data"
+		}
+	};
+};
---
+++ scripts/mobile-menu.js
@@ -0,0 +1,21 @@
+/* Mobile Menu Transition
+
+const hamburger = document.querySelector('.hamburger');
+const navMain = document.querySelector('.nav-main');
+
+
+hamburger.addEventListener("click", () => {
+	navMain.classList.toggle('open');
+}); 
+
+*/
+
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
